84509aef46d6111d4762752cf346bbba1f2f9a73
vedhagnanam <vetha.gnanam98@gmail.com>
2019-12-12 00:57:30 +0530

    added delete route

--- routes/users.js
+++ routes/users.js
@@ -20,4 +20,8 @@ let {id,salary} = req.body;
 user.update({salary},{where:{id:id}}).then(res.json({msg:"success"}))
 
 })
+router.delete('/',(req,res)=>{
+    let{id}=req.body;
+    user.destroy({where:{id:id}}).then(res.json({msg:'deleted successfully'}))
+})
 module.exports = router
